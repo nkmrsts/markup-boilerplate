3cf359500c63cead04bb77ecabfc423b3eab1b61
nkmrsts <v1llnaxx@gmail.com>
2019-01-29 16:01:04 +0900

    css内の画像を処理

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin  = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-module.exports = (env,argv) => {
-  const IS_DEVELOPMENT = argv.mode === 'development';
+module.exports = (env, argv) => {
+  const IS_DEVELOPMENT = argv.mode === 'development'
   return {
-      // エントリーポイント
+    // エントリーポイント
     entry: `./src/index.js`,
 
     output: {
@@ -24,20 +25,24 @@ module.exports = (env,argv) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
               presets: [
-                ['@babel/preset-env',{
-                  "targets": ["> 1%", "last 2 versions", "not ie <= 8"]
-              }]
-            ]}
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: ['> 1%', 'last 2 versions', 'not ie <= 8']
+                  }
+                ]
+              ]
+            }
           }
         },
         {
           test: /\.html$/,
           use: [
             {
-              loader: "html-loader"
+              loader: 'html-loader'
             }
           ]
         },
@@ -45,28 +50,31 @@ module.exports = (env,argv) => {
           test: /\.scss/,
           use: [
             {
-              loader: MiniCssExtractPlugin.loader,
-            }, {
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
               // CSSをバンドルするための機能
               loader: 'css-loader',
               options: {
-                url: false, // CSS内のurl()メソッドの取り込みを禁止する
+                url: true, // CSS内のurl()メソッドの取り込みを禁止する
                 sourceMap: IS_DEVELOPMENT,
                 // 0 => no loaders (default);
                 // 1 => postcss-loader;
                 // 2 => postcss-loader, sass-loader
                 importLoaders: 2
-              },
-            }, {
+              }
+            },
+            {
               // PostCSSの設定
               loader: 'postcss-loader',
               options: {
                 sourceMap: IS_DEVELOPMENT
-              },
-            }, {
+              }
+            },
+            {
               loader: 'sass-loader',
               options: {
-                sourceMap: IS_DEVELOPMENT,
+                sourceMap: IS_DEVELOPMENT
               }
             }
           ]
@@ -74,34 +82,37 @@ module.exports = (env,argv) => {
         {
           test: /\.(jpe?g|png|gif|svg|ico)(\?.+)?$/,
           use: [
-              {
-                  loader: 'file-loader',
-                  options: {
-                      name: 'images/[name].[ext]'
-                  }
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'images/[name].[ext]'
               }
+            }
           ]
         }
       ]
     },
-    devtool: "source-map",
+    devtool: 'source-map',
     optimization: {
       minimizer: [
-          new OptimizeCSSAssetsPlugin({
-              cssProcessorOptions: { discardComments: { removeAll: true } }
-          })
+        new UglifyJSPlugin({
+          uglifyOptions: { compress: { drop_console: true }, sourceMap: false }
+        }),
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: { discardComments: { removeAll: true } }
+        })
       ]
     },
     plugins: [
       new MiniCssExtractPlugin({
         // Options similar to the same options in webpackOptions.output
         // both options are optional
-        filename: "style.css"
+        filename: 'style.css'
       }),
       new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html"
+        template: './src/index.html',
+        filename: './index.html'
       })
     ]
   }
-};
+}
